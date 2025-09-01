1f6c2d17aa6f062c4b4f94ab99ca20972e0e981d
molengeek <saoussan.elk@gmail.com>
2025-09-01 11:11:53 +0200

    migrations

--- database/migrations/2025_09_01_075323_create_equipes_table.php
+++ database/migrations/2025_09_01_075323_create_equipes_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('equipes', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('city');
+            $table->foreignId('continent_id');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
--- database/migrations/2025_09_01_075346_create_joueurs_table.php
+++ database/migrations/2025_09_01_075346_create_joueurs_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('joueurs', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('age');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('city');
+            $table->foreignId('equipe_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('position_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('genre_id')->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
