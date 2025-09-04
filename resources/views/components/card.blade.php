81063e9d9bf0dfc443b24ad6d5e31b28063f7d53
molengeek <saoussan.elk@gmail.com>
2025-09-04 16:26:25 +0200

    la

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,21 +6,21 @@
     'link' => null
 ])
 
-    <div class="card h-100 d-flex flex-column justify-content-between rounded-2">
-        <div class="d-flex justify-content-center align-items-center card_div_img" style="height: 300px;">
-            <img src="{{ $image ?? 'https://placehold.co/400' }}" class="card_img" alt="">
-        </div>
-            @if ($element)
-                @if($element instanceof \App\Models\Equipe)
-                    @can('update-equipe', $element)
-                        <a href="{{ route('back.equipe.edit', $element->id) }}" class="btn_modify">Modify Team</a>
-                    @endcan
-                @elseif($element instanceof \App\Models\Joueur)
-                    @can('update-joueur', $element)
-                        <a href="{{ route('back.joueur.edit', $element->id) }}" class="btn_modify">Modify Player</a>
-                    @endcan
-                @endif
+<div class="card h-100 d-flex flex-column justify-content-between rounded-2">
+    <div class="d-flex justify-content-center align-items-center card_div_img" style="height: 300px;">
+        <img src="{{ $image ?? 'https://placehold.co/400' }}" class="card_img" alt="">
+    </div>
+        @if ($element)
+            @if($element instanceof \App\Models\Equipe)
+                @can('update-equipe', $element)
+                    <a href="{{ route('back.equipe.edit', $element->id) }}" class="btn_modify">Modify Team</a>
+                @endcan
+            @elseif($element instanceof \App\Models\Joueur)
+                @can('update-joueur', $element)
+                    <a href="{{ route('back.joueur.edit', $element->id) }}" class="btn_modify">Modify Player</a>
+                @endcan
             @endif
+        @endif
     <div class="card-body mt-auto">
         <h5 class="card-title mb-1">{{ $title }}</h5>
         <p class="card-text mb-2">{{ $subtitle }}</p>
